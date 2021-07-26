0442cd58239968023c88a8e59676873e5e4bdc4b
MarkReed2 <>
2021-07-26 17:44:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen #import the turutle and screen classes from the turtle library
+
+t: Turtle = Turtle() #the name of the turtle variable.  variables do not need to be declared by type byr rtoe hnting can be used for human sake
+s: Screen = Screen() #Create a screen variable
+
+t.forward(150) #tells the turtle to move forward 150
+
+t.right(90) #rotates turtle right 90 degrees
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+
+
+
+
+
+
+
+
+
